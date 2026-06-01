b46fbfa7019b701dec95168f1f8e84b125f3e7db
1958uf <115398121+1958uf@users.noreply.github.com>
2026-06-01 10:53:07 +0800

    Update TestNvidiaAPI.py

--- TestNvidiaAPI.py
+++ TestNvidiaAPI.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = "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"
+  api_key = ""
 )
 
 completion = client.chat.completions.create(
